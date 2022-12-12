8a07a80a525f01f54e076ee1a1b9039438081f21
d4vc-198 <contactodiegoavc@gmail.com>
2022-12-12 12:27:49 -0600

    monty.h update

--- monty.h
+++ monty.h
@@ -31,6 +31,24 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern char *line;
+void opcomp(stack_t **stack, unsigned int line_number, char *opcode);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+
+
+/**
+ * struct GlobalStruct - contains all globals
+ * @data: int for push
+ *
+ */
+struct GlobalStruct
+{
+	int data;
+} globes;
 
 #endif
